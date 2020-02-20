f1357717216f1f2864c2b87aae02090741144302
Yiqi Zhou <yiqizhou@andrew.cmu.edu>
2020-02-20 17:17:32 -0500

    tweak google strategy settings

--- services/passport.js
+++ services/passport.js
@@ -26,7 +26,8 @@ passport.use(
 		{
 			clientID: keys.googleClientID,
 			clientSecret: keys.googleClientSecret,
-			callbackURL: '/auth/google/callback'
+			callbackURL: '/auth/google/callback',
+			proxy: true
 		}, 
 		// this arrow function is to take this identifying user information and save it
 		// to our database
