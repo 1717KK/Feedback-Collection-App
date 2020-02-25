191fcca7c043962ee3d2c59086b19ac499f1512c
1717KK <yiqi402@gmail.com>
2020-02-24 23:25:48 -0500

    update all client side survey form set up

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -9,9 +9,17 @@ export const fetchUser = () => async dispatch => {
 
 // a new action creator: will be called with the token that we
 // just got back from Stripe API
-export const handleToken = (token) => async dispatch => {
+export const handleToken = token => async dispatch => {
 	// make a post request to our back end server and we've gotten a response
 	const res = await axios.post('/api/stripe', token);
 	
+	dispatch({ type: FETCH_USER, payload: res.data });
+};
+
+export const submitSurvey = (values, history) =>async dispatch => {
+	const res = await axios.post('/api/surveys', values);
+
+	// redirect to '/surveys'
+	history.push('/surveys');
 	dispatch({ type: FETCH_USER, payload: res.data });
 };
